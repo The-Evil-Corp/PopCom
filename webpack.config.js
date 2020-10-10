aca4a3c57da4924b4f0e68ba650bce4e64322965
sudo-nan0-RaySK <saksham.sethi2017@vitstudent.ac.in>
2020-10-10 14:25:40 +0530

    fix: Fixed Babel versioning issue

--- webpack.config.js
+++ webpack.config.js
@@ -21,9 +21,11 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                loader:'babel-loader',
-                query: {
-                    presets: ['es2015', 'react']
+                use:{
+                    loader:'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env','react']
+                    }
                 }
             },
             { 
